1484ff6088e7306081e3ff3b28ef8720068c43c8
Song Li <da_tou_li@yahoo.com>
2020-01-09 21:15:43 -0500

    Add a test on array sorting

--- src/test/java/org/song/example/List_Sorting_test.java
+++ src/test/java/org/song/example/List_Sorting_test.java
@@ -1,11 +1,35 @@
 package org.song.example;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class List_Sorting_test {
 	
 	@Test
-	public void TestSorting() {
+	public void Test_Sort_Array() {
+		Integer[] array = {3, 1, 2, 8};
+		Integer[] clone = array.clone();
+		
+		// Asc
+		Arrays.sort(array);
+		
+		int v = array[0];
+		for (int i = 1; i < array.length; i++) {
+			Assert.assertTrue(array[i].intValue() >= v);
+			v = array[i];
+		}
+		
+		// Desc
+		Arrays.sort(array, Collections.reverseOrder());
+		
+		v = array[0];
+		for (int i = 1; i < array.length; i++) {
+			Assert.assertTrue(array[i].intValue() <= v);
+			v = array[i];
+		}
 		
 	}
 }
